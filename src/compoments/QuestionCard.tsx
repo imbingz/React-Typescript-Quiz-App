7ef4ccf28de3ffc90129b2beb073cd679380102d
Bing Z <bingz@me.com>
2021-02-08 14:01:21 -0700

    defined props type on questionCard cmpt

---
+++ src/compoments/QuestionCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+//create type for props (can be any name)
+type Props = {
+	question: string;
+	answers: string[];
+	callback: any;
+	userAnswer: any;
+	questionNumber: number;
+	totalQuestions: number;
+};
+
+// need to specify the component is function component (React.FC)
+// pass the Props we defined
+//deconstruct the Props in parameter
+const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNumber, totalQuestions }) => (
+	<div>
+		<p className='number'>
+			Questions: {questionNumber} / {totalQuestions}
+		</p>
+		<p dangerouslySetInnerHTML={{ __html: question }} />
+		<div>
+			{answers.map(answer => (
+				<div>
+					<button disabled={userAnswer} onClick={callback} />
+					<span dangerouslySetInnerHTML={{ __html: answer }} />
+				</div>
+			))}
+		</div>
+	</div>
+);
+
+export default QuestionCard;

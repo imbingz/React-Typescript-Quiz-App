32e948a0bec338aa921203792b4c08da6d5ab174
Bing Z <bingz@me.com>
2021-02-09 12:47:38 -0700

    added Question type n answers prop to trivia data

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,19 @@
+//import suffled array util
+import { shuffleArray } from './utils';
+
+//define question type
+export type Question = {
+	category: string;
+	correct_answer: string;
+	difficulty: string;
+	incorrect_answers: string[];
+	question: string;
+	type: string;
+};
+
+// define question State type - add answers type (both correct and incorrect answers)
+export type QuestionState = Question & { answers: string[] };
+
 // use enum to describe difficulty types
 export enum Difficulty {
 	EASY = 'easy',
@@ -9,11 +25,13 @@ export const fetchTrivia = async (amount: number, difficulty: Difficulty) => {
 	//config endpoint
 	const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
 
-	console.log(endpoint);
-
 	// fetch trivia
 	const resp = await fetch(endpoint);
 	const data = await resp.json();
 
-	console.log(data);
+	//return n map through trivia array and randomize the array of answers
+	return data.results.map((question: Question) => ({
+		...question,
+		answers: shuffleArray([ question.correct_answer, ...question.incorrect_answers ])
+	}));
 };

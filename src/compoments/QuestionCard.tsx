7bd42bbb64fa57184d57387b2c6fb1070d049012
Bing Z <bingz@me.com>
2021-02-10 16:14:25 -0700

    styled question card

--- src/compoments/QuestionCard.tsx
+++ src/compoments/QuestionCard.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-
 //types
 import { AnswerObj } from '../App';
+//styles
+import { Wrapper, ButtonWrapper } from './QuestionCard.style';
 
 //create type for props (can be any name)
 //https://stackoverflow.com/questions/54433183/typescript-interface-signature-for-the-onclick-event-in-reactjs
@@ -18,24 +19,27 @@ type Props = {
 // pass the Props we defined
 //deconstruct the Props in parameter
 const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNumber, totalQuestions }) => (
-	<div>
+	<Wrapper>
 		<p className='number'>
 			Questions: {questionNumber} / {totalQuestions}
 		</p>
 		<p dangerouslySetInnerHTML={{ __html: question }} />
 		<div>
 			{answers.map((answer, i) => (
-				<div key={i}>
+				<ButtonWrapper key={i}
+                correct={userAnswer?.correctAnswer === answer} //use optional chaining to avoid error if no userAnswer
+                userClicked={userAnswer?.answer === answer}
+                >
 					<button
 						disabled={userAnswer ? true : false} // or !!userAnswer
 						onClick={callback}
 						dangerouslySetInnerHTML={{ __html: answer }}
 						value={answer}
 					/>
-				</div>
+				</ButtonWrapper>
 			))}
 		</div>
-	</div>
+	</Wrapper>
 );
 
 export default QuestionCard;
